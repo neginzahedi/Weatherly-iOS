96d2acd5eb950bcd02ef4d9a7ac7a64c8409d759
Negin Zahedi <zahedi.negin@gmail.com>
2022-06-25 17:49:53 -0400

    Initial Commit

---
+++ Weatherly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weatherly
+//
+//  Created by Negin Zahedi on 2022-06-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
