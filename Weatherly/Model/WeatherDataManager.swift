80fc8384520d8964b43bf285b64ca84b01669ccf
Negin Zahedi <zahedi.negin@gmail.com>
2022-07-03 12:15:12 -0400

    json data retrived

--- Weatherly/Model/WeatherDataManager.swift
+++ Weatherly/Model/WeatherDataManager.swift
@@ -4,6 +4,16 @@
 //
 //  Created by Negin Zahedi on 2022-06-30.
 //
+// Network
+// 1. create URL
+// 2. create URL session
+// 3. give URL a task (like browser to fetch data)
+// 4. start the task (hit enter in search bar)
+//
+// Closure : anonymos function - function without a name
+//
+
+
 
 import Foundation
 
@@ -15,5 +25,39 @@ struct WeatherDataManager{
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
         print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String){
+        // 1. create URL : location of the resource (can be on local or remote server)
+        if let url = URL(string: urlString){
+            
+            // 2. create URL session (downloading or uploading data)
+            let session = URLSession(configuration: .default)
+            
+            // 3. create task - give URL a task (to retrive content of url
+            let task = session.dataTask(with: url, completionHandler: handler(data: response: error: ))
+            
+            // 4. start the task (hit enter in search bar)
+            task.resume()
+        }
+        
+        
+    }
+    
+    func handler(data: Data?, response: URLResponse?, error: Error?) -> Void{
+        // if there are errors
+        if error != nil{
+            print(error!)
+            // empty return just means exit
+            return
+        }
+        
+        // if there is no errors
+        if let safeData = data{
+            // for encoding text from website use .utf8
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
