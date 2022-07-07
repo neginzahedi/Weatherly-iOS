19f661a2411b8793e1a609df4e4d496f523746ae
Negin Zahedi <zahedi.negin@gmail.com>
2022-07-07 12:02:37 -0400

    changed to Codable

--- Weatherly/Model/WeatherData.swift
+++ Weatherly/Model/WeatherData.swift
@@ -7,18 +7,23 @@
 // decodable protocol
 // name in struct should match the property name of JSON
 
+// Encodable: encode to json
+// Codable: Rncodable and Decodable
+
 import Foundation
 
-struct WeatherData: Decodable {
+struct WeatherData: Codable{
     let name: String
     let main: Main
     let weather: [Weather]  //array in json
+    
 }
 
-struct Main: Decodable{
+struct Main: Codable{
     let temp: Double
 }
 
-struct Weather: Decodable{
+struct Weather: Codable{
     let description: String
+    let id: Int
 }
